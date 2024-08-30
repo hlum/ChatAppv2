ea7fefd5a0a07c499644a98f201c9d32bf684bd9
アウン <hlumaungphyo@gmail.com>
2024-08-31 00:01:51 +0900

    Initial Commit

---
+++ ChatAppv2/ChatAppv2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppv2App.swift
+//  ChatAppv2
+//
+//  Created by Hlwan Aung Phyo on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
