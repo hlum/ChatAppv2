25e6e6868100c690f361aa673a8e0f31d8de34e9
アウン <hlumaungphyo@gmail.com>
2024-09-17 04:34:47 +0900

    bug fix
    showing unread sign in the mainview when the sender is the user bug ifxed

--- ChatAppv2/ChatAppv2App.swift
+++ ChatAppv2/ChatAppv2App.swift
@@ -10,12 +10,27 @@ import Firebase
 
 @main
 struct ChatAppv2App: App {
-    init(){
-        FirebaseApp.configure()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    init() {
+        FirebaseApp.configure() // Configure Firebase once
     }
+
     var body: some Scene {
         WindowGroup {
             MainMessageView()
         }
     }
+
+    // AppDelegate class that conforms to UIApplicationDelegate
+    class AppDelegate: NSObject, UIApplicationDelegate {
+        
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            // Your custom configuration
+            return true
+        }
+    }
 }
+
+
+
