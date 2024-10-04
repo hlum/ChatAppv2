60c9dd606875729ce8383842f17cd13df59399d9
アウン <hlumaungphyo@gmail.com>
2024-10-04 11:21:20 +0900

    Created LottieView

---
+++ ChatAppv2/Manager/LottieView.swift
@@ -0,0 +1,27 @@
+//
+//  LottieView.swift
+//  ChatAppv2
+//
+//  Created by Hlwan Aung Phyo on 10/4/24.
+//
+
+import Lottie
+import SwiftUI
+
+struct LottieView:UIViewRepresentable{
+    
+    var animationFileName:String
+    let loopMode:LottieLoopMode
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+    
+    func makeUIView(context: Context) -> Lottie.LottieAnimationView {
+        let animationView = LottieAnimationView(name:animationFileName)
+        animationView.loopMode = loopMode
+        animationView.play()
+        animationView.contentMode = .scaleAspectFit
+        return animationView
+        
+    }
+}
